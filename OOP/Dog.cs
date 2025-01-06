cc590f4590ee21f61ecec252ffd234992a898744
TranKienCuong2003 <trankiencuong30072003@gmail.com>
2025-01-06 23:57:29 +0700

    Update Dog.cs file

--- OOP/Dog.cs
+++ OOP/Dog.cs
@@ -1,22 +1,22 @@
-// Dog.cs
-using System;
-
 namespace OOP
 {
-    public class Dog : Animal
+    public class Dog
     {
-        public Dog(string name) : base(name)
+        public string Name { get; set; }
+
+        public Dog(string name)
         {
+            Name = name;
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine($"{Name} đang kêu...");
         }
 
         public void Bark()
         {
-            Console.WriteLine($"{Name} barks.");
-        }
-
-        public override void Speak()
-        {
-            Console.WriteLine($"{Name} says Woof!");
+            Console.WriteLine($"{Name} đang sủa!");
         }
     }
 }
